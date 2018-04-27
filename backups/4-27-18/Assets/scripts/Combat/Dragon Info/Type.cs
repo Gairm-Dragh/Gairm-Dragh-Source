ed593c66ec1e744be485cddded625c8f64e458ac
AvisTonitrui <bugaboo421@gmail.com>
2018-04-27 15:06:59 -0700

    Added backup.

---
+++ backups/4-27-18/Assets/scripts/Combat/Dragon Info/Type.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Type { //The type class is used for keeping track of the base stats of each type
+    public int HP, ATK, DEF, SPA, SPD, SPE; //The base stats of the type
+    public int typeIndex; //The index for the type
+    public string name; //The name of the type
+
+    //initializer for the class
+    public Type(int H, int A, int D, int SA, int SD, int SP, int ind, string nam) {
+        HP = H;
+        ATK = A;
+        DEF = D;
+        SPA = SA;
+        SPD = SD;
+        SPE = SP;
+        typeIndex = ind;
+        name = nam;
+    }
+}
---
+++ backups/4-27-18/Assets/scripts/menus/Main/mainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainMenu : MonoBehaviour { //this is used to run the main menu
+
+    public void toList() {
+        SceneManager.LoadScene("lobby");
+    }
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
