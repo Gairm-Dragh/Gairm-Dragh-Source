55dbe9d37555e21d42705f79d4bd51fda5fd136b
AvisTonitrui <bugaboo421@gmail.com>
2018-05-01 14:41:41 -0700

    I think I can pass info from lobby to game now.

---
+++ Gairm Dragh/Assets/scripts/Combat/playerInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerInfo : MonoBehaviour { //This class holds the information of a player
+
+    public string playerName;
+    public int playerTeam;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
