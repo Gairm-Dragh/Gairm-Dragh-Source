a6b90f5d2a8103a660c3c5ebedbd9e5ae43c0355
AvisTonitrui <bugaboo421@gmail.com>
2018-05-03 14:36:38 -0700

    Trying to figure out the order networking operations need to go.

--- Gairm Dragh/Assets/Lobby/Scripts/Lobby/LobbyHook.cs
+++ Gairm Dragh/Assets/Lobby/Scripts/Lobby/LobbyHook.cs
@@ -21,7 +21,7 @@ namespace Prototype.NetworkLobby {
             }
 
             DragonGlobals.initTypes();
-            //gamePlayer.GetComponent<playerInfo>().randomTeam();
+            Debug.Log("Hooked");
         }
     }
 
