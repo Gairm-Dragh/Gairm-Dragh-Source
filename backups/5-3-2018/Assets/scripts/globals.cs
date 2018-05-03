e961e14700950a5e2f5979ce93e04ce7553bb2e2
AvisTonitrui <bugaboo421@gmail.com>
2018-05-03 15:25:36 -0700

    Zipped the backup.

--- backups/5-3-2018/Assets/scripts/globals.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public static class globals{
-    public static int players = 0; //The number of players in the game
-}
