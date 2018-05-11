adb5621ce80b62431580bc6ae093d9f399b3602c
AvisTonitrui <bugaboo421@gmail.com>
2018-05-10 17:28:09 -0700

    Things are possibly solved.

--- Gairm Dragh/Assets/scripts/menus/Lobby/nameHolder.cs
+++ Gairm Dragh/Assets/scripts/menus/Lobby/nameHolder.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Prototype.NetworkLobby;
 
 public class nameHolder : MonoBehaviour {
-    public GameObject player;
+    public GameObject playerList; //The player list
+    public GameObject player; //The player
+    public GameObject self; //This game object
 
     // Use this for initialization
     void Start() {
@@ -12,6 +15,21 @@ public class nameHolder : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-
+        if (player == self) {
+            //Debug.Log("Equals");
+            for (int i = 0; i < playerList.transform.childCount - 1; i++) {
+                if (playerList.transform.GetChild(i).GetChild(6).gameObject.activeInHierarchy) {
+                    Debug.Log("Found the player");
+                    player = playerList.transform.GetChild(i).gameObject;
+                    break;
+                }
+                else {
+                    Debug.Log("Still Looking");
+                }
+            }
+        }
+        else {
+            globals.name = player.GetComponent<LobbyPlayer>().playerName;
+        }
     }
 }
