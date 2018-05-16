09934a376203cdc5162b89e850a48ff32f5fe7f9
AvisTonitrui <bugaboo421@gmail.com>
2018-05-16 15:27:18 -0700

    Working on a bunch of manual selecting of button stuff.

--- Gairm Dragh/Assets/scripts/Combat/UIControl.cs
+++ Gairm Dragh/Assets/scripts/Combat/UIControl.cs
@@ -6,7 +6,18 @@ using UnityEngine.UI;
 
 public class UIControl : NetworkBehaviour {
     public GameObject dragonName; //The text that holds the dragon name
+
+    [SyncVar]
     public string command = ""; //The command for this slot
+
+    public GameObject move1, move2, move3, move4; //The buttons with the moves
+    public GameObject switch1, switch2, switch3, switch4; //The buttons with the dragons to switch
+    GameObject[] commandList = new GameObject[8];
+    public GameObject[] UIList = new GameObject[8]; //The other UIs
+    public bool dead = false; //Whether or not this slot is dead
+    string temp = ""; //the temporary string for the command
+    public int slot; //The number of this slot
+    public int team; //The team the dragon is on
 
     [SyncVar(hook = "OnOwnerChange")]
     public string owner; //The person who controls this UI
@@ -19,9 +30,68 @@ public class UIControl : NetworkBehaviour {
         dragonName.GetComponent<Text>().text = newName;
     }
 
+    public string getRandomTarget() {
+        string target = "";
+        int targetNum = 0;
+        bool found = false;
+
+        while (!found) {
+            targetNum = Random.Range(0, 3);
+
+            if (team == 1) {
+                targetNum += 4;
+            }
+
+            try {
+                if (!UIList[targetNum].GetComponent<UIControl>().dead) {
+                    found = true;
+                }
+            } catch {
+                found = false;
+            }
+            
+        }
+
+        target = targetNum.ToString();
+
+        return target;
+    }
+
+    public void onTemp(int commandIndex) {
+        temp = ""; //The temporary command, we don't changecommand until filled out
+
+        if (dead) {
+            temp = "true";
+        } else {
+            temp = "false,";
+
+            if (commandIndex > 4) {
+                temp += "true,";
+                temp += (commandIndex - 5).ToString();
+                temp += "," + slot.ToString();
+            } else {
+                temp += "false,";
+                temp += commandList[commandIndex].GetComponent<command>().index;
+                temp += ",";
+
+                temp += getRandomTarget();
+            }
+
+        }
+
+        command = temp;
+    }
+
     // Use this for initialization
     void Start() {
-
+        commandList[0] = move1;
+        commandList[1] = move2;
+        commandList[2] = move3;
+        commandList[3] = move4;
+        commandList[4] = switch1;
+        commandList[5] = switch2;
+        commandList[6] = switch3;
+        commandList[7] = switch4;
     }
 
     // Update is called once per frame
---
+++ Gairm Dragh/Assets/scripts/Combat/command.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class command : MonoBehaviour {
+    public int slot; //The number of this slot
+    public int index; //The index of the move/dragon this button currently shows
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
